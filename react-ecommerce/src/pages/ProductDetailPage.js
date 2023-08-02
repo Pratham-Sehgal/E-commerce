d11ae07c7141104c98343a9d0ae33c300c75b508
Pratham Sehgal <prathamsehgal@Prathams-MacBook-Air.local>
2023-08-02 11:43:33 +0530

    final commit

---
+++ react-ecommerce/src/pages/ProductDetailPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../features/navbar/Navbar'
+import ProductDetail from '../features/product/components/ProductDetail'
+const ProjectDetailPage = () => {
+  return (
+    <div>
+        <Navbar>
+            <ProductDetail />
+        </Navbar>
+    </div>
+  )
+}
+
+export default ProjectDetailPage
